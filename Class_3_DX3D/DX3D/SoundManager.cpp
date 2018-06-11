3b8da4054b1aa9116f7804c76746836d015bbe48
김민수 <keltone54@gmail.com>
2018-06-11 19:14:35 +0900

    병합

--- Class_3_DX3D/DX3D/SoundManager.cpp
+++ Class_3_DX3D/DX3D/SoundManager.cpp
@@ -23,20 +23,27 @@ enum SoundType
 SoundManager::SoundManager()
 {
 	m_pBGSound = NULL;
-	m_pShotSound_1 = NULL;
+	m_pAmbient = NULL;
+
+	m_pShot_1 = NULL;
+	m_pReload = NULL;
 	m_pWalk_Dirt = NULL;
 	m_pRun_Dirt = NULL;
 	m_pGear_Walk = NULL;
 
 	walkInterval = 0;
 	runInterval = 0;
+	reloadInterval = 0;
 }
 
 
 SoundManager::~SoundManager()
 {
 	m_pBGSound->ReleaseSound();
-	m_pShotSound_1->ReleaseSound();
+	m_pAmbient->ReleaseSound();
+
+	m_pShot_1->ReleaseSound();
+	m_pReload->ReleaseSound();
 	m_pWalk_Dirt->ReleaseSound();
 	m_pRun_Dirt->ReleaseSound();
 	m_pGear_Walk->ReleaseSound();
@@ -44,32 +51,38 @@ SoundManager::~SoundManager()
 
 void SoundManager::soundList()
 {
-	s_music_file.push_back("MoDD");
-	s_music_file.push_back("Test");
+	// 배경음
+	s_music_f.push_back("MoDD");
+	s_music_f.push_back("Test");
 
-	s_shot_1_file.push_back("Kar98az1p");
-	s_shot_1_file.push_back("Kar98az2p");
+	// 환경음
+	s_ambient_f.push_back("ambient_test_4");
 
 
-	s_walk_dirt_file.push_back("Boot_dirt_walk1");
-	s_walk_dirt_file.push_back("Boot_dirt_walk2");
-	s_walk_dirt_file.push_back("Boot_dirt_walk3");
-	s_walk_dirt_file.push_back("Boot_dirt_walk4");
-	s_walk_dirt_file.push_back("Boot_dirt_walk5");
-	s_walk_dirt_file.push_back("Boot_dirt_walk6");
+	// 총소리
+	s_shot_1_f.push_back("Kar98az1p");
+	s_shot_1_f.push_back("Kar98az2p");
 
-	s_run_dirt_file.push_back("Boot_dirt_run1");
-	s_run_dirt_file.push_back("Boot_dirt_run2");
-	s_run_dirt_file.push_back("Boot_dirt_run3");
-	s_run_dirt_file.push_back("Boot_dirt_run4");
-	s_run_dirt_file.push_back("Boot_dirt_run5");
-	s_run_dirt_file.push_back("Boot_dirt_run6");
+	// 재장전
+	s_reload_f.push_back("reload7");
 
-	s_gear_walk_file.push_back("Gear_Walk1");
-	s_gear_walk_file.push_back("Gear_Walk2");
-	s_gear_walk_file.push_back("Gear_Walk3");
-	s_gear_walk_file.push_back("Gear_Walk4");
-	s_gear_walk_file.push_back("Gear_Walk5");
+	// 발소리
+	s_walk_dirt_f.push_back("Boot_dirt_walk1");
+	s_walk_dirt_f.push_back("Boot_dirt_walk2");
+	s_walk_dirt_f.push_back("Boot_dirt_walk3");
+	s_walk_dirt_f.push_back("Boot_dirt_walk4");
+	s_walk_dirt_f.push_back("Boot_dirt_walk5");
+	s_walk_dirt_f.push_back("Boot_dirt_walk6");
+
+	s_run_dirt_f.push_back("Boot_dirt_run1");
+	s_run_dirt_f.push_back("Boot_dirt_run2");
+	s_run_dirt_f.push_back("Boot_dirt_run3");
+	s_run_dirt_f.push_back("Boot_dirt_run4");
+	s_run_dirt_f.push_back("Boot_dirt_run5");
+	s_run_dirt_f.push_back("Boot_dirt_run6");
+
+	s_gear_walk_f.push_back("Gear_Walk1");
+	s_gear_walk_f.push_back("Gear_Walk4");
 }
 
 void SoundManager::setMP3(string folder, string * s_name, vector<string> s_name_file)
@@ -98,12 +111,14 @@ void SoundManager::createSound()
 {
 	soundList();
 
-	CreateMP3(m_pBGSound, "Music/", s_music, s_music_file, MUSIC);
-	CreateWAV(m_pShotSound_1, "Shot/", s_shot_1, s_shot_1_file, EFFECT);
-	CreateWAV(m_pWalk_Dirt, "Walk/", s_walk_dirt, s_walk_dirt_file, EFFECT);
-	CreateWAV(m_pRun_Dirt, "Run/", s_run_dirt, s_run_dirt_file, EFFECT);
-	CreateWAV(m_pGear_Walk, "Gear/", s_gear_walk, s_gear_walk_file, EFFECT);
+	CreateMP3(m_pBGSound, "Music/", s_music, s_music_f, MUSIC);
+	CreateWAV(m_pAmbient, "Ambient/", s_ambient, s_ambient_f, AMBIENT);
 
+	CreateWAV(m_pShot_1, "Shot/", s_shot_1, s_shot_1_f, EFFECT);
+	CreateWAV(m_pReload, "Reload/", s_reload, s_reload_f, EFFECT);
+	CreateWAV(m_pWalk_Dirt, "Walk/", s_walk_dirt, s_walk_dirt_f, EFFECT);
+	CreateWAV(m_pRun_Dirt, "Run/", s_run_dirt, s_run_dirt_f, EFFECT);
+	CreateWAV(m_pGear_Walk, "Gear/", s_gear_walk, s_gear_walk_f, EFFECT);
 
 }
 
@@ -117,22 +132,35 @@ void SoundManager::stopMusic(int soundNum)
 	m_pBGSound->StopSound(soundNum);
 }
 
+void SoundManager::playAmbient(int soundNum)
+{
+	m_pAmbient->PlaySound(soundNum);
+}
+
+void SoundManager::stopAmbient(int soundNum)
+{
+	m_pAmbient->StopSound(soundNum);
+}
+
 void SoundManager::ShotSound()
 {
-	int rN = rand() % s_shot_1_file.size();
-	m_pShotSound_1->PlaySound(rN);
+	int rN = rand() % s_shot_1_f.size();
+
+	m_pShot_1->PlaySound(rN);
+	m_pReload->PlaySound(0);
+
 }
 
 void SoundManager::WalkSound()
 {
 	int r6 = rand() % 6;
-	int r5 = rand() % 5;
+	int rGW = rand() % s_gear_walk_f.size();
 
 	walkInterval++;
 	if (walkInterval > 30)
 	{
 		m_pWalk_Dirt->PlaySound(r6);
-		m_pGear_Walk->PlaySound(r5);
+		m_pGear_Walk->PlaySound(rGW);
 
 		walkInterval = 0;
 	}
@@ -141,13 +169,13 @@ void SoundManager::WalkSound()
 void SoundManager::RunSound()
 {
 	int r6 = rand() % 6;
-	int r5 = rand() % 5;
+	int rGW = rand() % s_gear_walk_f.size();
 
 	runInterval++;
 	if (runInterval > 20)
 	{
 		m_pRun_Dirt->PlaySound(r6);
-		m_pGear_Walk->PlaySound(r5);
+		m_pGear_Walk->PlaySound(rGW);
 
 		runInterval = 0;
 	}
