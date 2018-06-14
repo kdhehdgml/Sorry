2a2b4f9ca21b4efba5fff0745b595802d20df035
영락 조 <jyl011202@gmail.com>
2018-06-14 18:16:01 +0900

    AI 스킨추가

---
+++ Class_3_DX3D/DX3D/TEAM_TEX.cpp
@@ -0,0 +1,311 @@
+#include "stdafx.h"
+#include "TEAM_TEX.h"
+#include "AllocateHierarchy.h"
+
+#define SCALE 4.0f
+
+
+TEAM_TEX::TEAM_TEX()
+{
+	m_baseRotY = D3DX_PI;
+
+	m_pRootFrame = NULL;
+	m_pAnimController = NULL;
+	m_fBlendTime = 0.3f;
+	m_fPassedBlendTime = 0.0f;
+	m_animIndex = 0;
+	m_bWireFrame = false;
+	m_bDrawFrame = true;
+	m_bDrawSkeleton = false;
+}
+
+
+TEAM_TEX::~TEAM_TEX()
+{
+	SAFE_RELEASE(m_pSphereMesh);
+	AllocateHierarchy alloc;
+	D3DXFrameDestroy(m_pRootFrame, &alloc);
+
+	SAFE_RELEASE(m_pAnimController);
+}
+
+void TEAM_TEX::Init()
+{
+
+	D3DXCreateSphere(g_pDevice, 0.01f, 10, 10, &m_pSphereMesh, NULL);
+
+	//Load(ASSET_PATH + _T("zealot/"), _T("zealot.X"));
+	//CString path = "resources/xFile/";
+	CString path = "resources/xFile/newMan/";
+	CString filename = "stand_idle.X";
+	Load(path, filename);
+	D3DXMatrixIdentity(&m_matWorld);
+
+	D3DXMatrixRotationY(&matR, D3DX_PI / 2);
+}
+
+void TEAM_TEX::Update()
+{
+
+	D3DXMatrixTranslation(&matT, m_pos.x, m_pos.y, m_pos.z);
+	D3DXMatrixScaling(&matS, SCALE, SCALE, SCALE);
+	UpdateAnim();
+	UpdateFrameMatrices(m_pRootFrame, NULL);
+
+	m_matWorld = matS * matR * matT;
+}
+
+void TEAM_TEX::Render()
+{
+	m_numFrame = 0;
+	m_numMesh = 0;
+	if (m_bDrawFrame)DrawFrame(m_pRootFrame);
+	if (m_bDrawSkeleton)DrawSkeleton(m_pRootFrame, NULL);
+}
+
+void TEAM_TEX::Load(LPCTSTR path, LPCTSTR filename)
+{
+	AllocateHierarchy alloc(path);
+
+	CString fullPath(path);
+	fullPath.Append(filename);
+
+	D3DXLoadMeshHierarchyFromX(fullPath, D3DXMESH_MANAGED, g_pDevice,
+		&alloc, NULL, &m_pRootFrame, &m_pAnimController);
+
+	SetupBoneMatrixPointers(m_pRootFrame);
+}
+
+void TEAM_TEX::SetupBoneMatrixPointers(LPD3DXFRAME pFrame)
+{
+	if (pFrame->pMeshContainer != NULL)
+	{
+		SetupBoneMatrixPointersOnMesh(pFrame->pMeshContainer);
+	}
+
+	if (pFrame->pFrameSibling != NULL)
+	{
+		SetupBoneMatrixPointers(pFrame->pFrameSibling);
+	}
+
+	if (pFrame->pFrameFirstChild != NULL)
+	{
+		SetupBoneMatrixPointers(pFrame->pFrameFirstChild);
+	}
+}
+
+void TEAM_TEX::SetupBoneMatrixPointersOnMesh(LPD3DXMESHCONTAINER pMeshContainerBase)
+{
+	DWORD numBones;
+	FRAME_EX* pFrameExInfluence;
+	MESHCONTAINER_EX* pMeshContainerEx = (MESHCONTAINER_EX*)pMeshContainerBase;
+
+	if (pMeshContainerEx->pSkinInfo != NULL)
+	{
+		numBones = pMeshContainerEx->pSkinInfo->GetNumBones();
+
+		for (DWORD i = 0; i < numBones; ++i)
+		{
+			pFrameExInfluence = (FRAME_EX*)D3DXFrameFind(m_pRootFrame,
+				pMeshContainerEx->pSkinInfo->GetBoneName(i));
+
+			pMeshContainerEx->ppBoneMatrixPtrs[i] = &pFrameExInfluence->CombinedTM;
+		}
+	}
+}
+
+void TEAM_TEX::UpdateAnim()
+{
+	float fDeltaTime = g_pTimeManager->GetDeltaTime();
+	// AdvanceTime 함수가 호출된 간격으로 Anim 키프레임 계산
+	m_pAnimController->AdvanceTime(fDeltaTime, NULL);
+
+	if (m_fPassedBlendTime <= m_fBlendTime)
+	{
+		m_fPassedBlendTime += fDeltaTime;
+
+		if (m_fPassedBlendTime < m_fBlendTime)
+		{
+			float fWeight = m_fPassedBlendTime / m_fBlendTime;
+
+			m_pAnimController->SetTrackWeight(0, fWeight);
+			m_pAnimController->SetTrackWeight(1, 1.0f - fWeight);
+		}
+		else
+		{
+			m_pAnimController->SetTrackWeight(0, 1);
+			m_pAnimController->SetTrackWeight(1, 0);
+			m_pAnimController->SetTrackEnable(1, false);
+		}
+	}
+
+}
+
+void TEAM_TEX::UpdateFrameMatrices(LPD3DXFRAME pFrame, LPD3DXFRAME pParent)
+{
+	FRAME_EX* pFrameEx = (FRAME_EX*)pFrame;
+
+	if (pParent != NULL)
+	{
+		pFrameEx->CombinedTM = pFrameEx->TransformationMatrix * ((FRAME_EX*)pParent)->CombinedTM;
+	}
+	else
+	{
+		pFrameEx->CombinedTM = pFrameEx->TransformationMatrix;
+	}
+
+	if (pFrameEx->pFrameSibling != NULL)
+	{
+		UpdateFrameMatrices(pFrameEx->pFrameSibling, pParent);
+	}
+
+	if (pFrameEx->pFrameFirstChild != NULL)
+	{
+		UpdateFrameMatrices(pFrameEx->pFrameFirstChild, pFrameEx);
+	}
+}
+
+void TEAM_TEX::DrawFrame(LPD3DXFRAME pFrame)
+{
+	m_numFrame++;
+
+	LPD3DXMESHCONTAINER pMeshContainer = pFrame->pMeshContainer;
+	while (pMeshContainer != NULL)
+	{
+		m_numMesh++;
+
+		DrawMeshContainer(pFrame);
+		pMeshContainer = pMeshContainer->pNextMeshContainer;
+	}
+
+	if (pFrame->pFrameSibling != NULL)
+	{
+		DrawFrame(pFrame->pFrameSibling);
+	}
+
+	if (pFrame->pFrameFirstChild != NULL)
+	{
+		DrawFrame(pFrame->pFrameFirstChild);
+	}
+}
+
+void TEAM_TEX::DrawMeshContainer(LPD3DXFRAME pFrame)
+{
+	if (pFrame->pMeshContainer->pSkinInfo == NULL)
+		return;
+
+	FRAME_EX* pFrameEx = (FRAME_EX*)pFrame;
+	MESHCONTAINER_EX* pMeshContainerEx = (MESHCONTAINER_EX*)pFrameEx->pMeshContainer;
+	DWORD numBones = pMeshContainerEx->pSkinInfo->GetNumBones();
+
+	for (DWORD i = 0; i < numBones; ++i)
+	{
+		pMeshContainerEx->pFinalBoneMatrices[i] =
+			pMeshContainerEx->pBoneOffsetMatrices[i] * *(pMeshContainerEx->ppBoneMatrixPtrs[i]);
+	}
+
+	PBYTE pVerticesSrc = NULL;
+	PBYTE pVerticesDest = NULL;
+
+	pMeshContainerEx->pOrigMesh->LockVertexBuffer(D3DLOCK_READONLY, (LPVOID*)&pVerticesSrc);
+	pMeshContainerEx->pWorkMesh->LockVertexBuffer(0, (LPVOID*)&pVerticesDest);
+
+	// generate skinned mesh
+	pMeshContainerEx->pSkinInfo->UpdateSkinnedMesh(
+		pMeshContainerEx->pFinalBoneMatrices, NULL, pVerticesSrc, pVerticesDest);
+
+	pMeshContainerEx->pWorkMesh->UnlockVertexBuffer();
+	pMeshContainerEx->pOrigMesh->UnlockVertexBuffer();
+
+	//g_pDevice->SetRenderState(D3DRS_LIGHTING, true);
+	if (m_bWireFrame)
+		g_pDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+
+	//D3DXMatrixIdentity(&m_matWorld);
+	//D3DXMatrixScaling(&m_matWorld, 5.0f, 5.0f, 5.0f);
+	g_pDevice->SetTransform(D3DTS_WORLD, &m_matWorld);
+
+	for (size_t i = 0; i < pMeshContainerEx->vecMtlTex.size(); ++i)
+	{
+		g_pDevice->SetMaterial(&pMeshContainerEx->vecMtlTex[i]->GetMaterial());
+		g_pDevice->SetTexture(0, pMeshContainerEx->vecMtlTex[i]->GetTexture());
+		pMeshContainerEx->pWorkMesh->DrawSubset(i);
+	}
+
+	g_pDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
+
+}
+
+void TEAM_TEX::DrawSkeleton(LPD3DXFRAME pFrame, LPD3DXFRAME pParent)
+{
+	FRAME_EX* pFrameEx = (FRAME_EX*)pFrame;
+	FRAME_EX* pParentFrameEx = (FRAME_EX*)pParent;
+
+	g_pDevice->SetTransform(D3DTS_WORLD, &(pFrameEx->CombinedTM));
+	//g_pDevice->SetTransform(D3DTS_WORLD, &(pFrameEx->CombinedTM * m_matWorld));
+
+	g_pDevice->SetMaterial(&DXUtil::WHITE_MTRL);
+	g_pDevice->SetTexture(0, NULL);
+	m_pSphereMesh->DrawSubset(0);
+
+	if (pParent != NULL && pFrame->Name != NULL && pParent->Name != NULL)
+	{
+		D3DXMATRIXA16 matThis = pFrameEx->CombinedTM;
+		D3DXMATRIXA16 matParent = pParentFrameEx->CombinedTM;
+
+		D3DXVECTOR3 posThis(matThis(3, 0), matThis(3, 1), matThis(3, 2));
+		D3DXVECTOR3 posParent(matParent(3, 0), matParent(3, 1), matParent(3, 2));
+
+		vector<VERTEX_PC> line{ VERTEX_PC(posThis, BLUE), VERTEX_PC(posParent, YELLOW) };
+		g_pDevice->SetRenderState(D3DRS_LIGHTING, false);
+		g_pDevice->SetFVF(VERTEX_PC::FVF);
+		D3DXMATRIXA16 mat;
+		D3DXMatrixIdentity(&mat);
+
+		g_pDevice->SetTransform(D3DTS_WORLD, &mat);
+		g_pDevice->DrawPrimitiveUP(D3DPT_LINELIST, 1, &line[0], sizeof(VERTEX_PC));
+		g_pDevice->SetRenderState(D3DRS_LIGHTING, false);
+	}
+
+	if (pFrame->pFrameSibling != NULL)
+	{
+		DrawSkeleton(pFrame->pFrameSibling, pParent);
+	}
+
+	if (pFrame->pFrameFirstChild != NULL)
+	{
+		DrawSkeleton(pFrame->pFrameFirstChild, pFrame);
+	}
+}
+
+void TEAM_TEX::SetAnimationIndex(int nIndex, bool isBlend)
+{
+	LPD3DXANIMATIONSET pNextAnimSet = NULL;
+	m_pAnimController->GetAnimationSet(nIndex, &pNextAnimSet);
+	//isBlend = false;
+	if (isBlend)
+	{
+		m_fPassedBlendTime = 0.0f;
+
+		LPD3DXANIMATIONSET pPrevAnimSet = NULL;
+
+		m_pAnimController->GetTrackAnimationSet(0, &pPrevAnimSet);
+		m_pAnimController->SetTrackAnimationSet(1, pPrevAnimSet);
+
+		D3DXTRACK_DESC trackDesc;
+		m_pAnimController->GetTrackDesc(0, &trackDesc);
+		m_pAnimController->SetTrackDesc(1, &trackDesc);
+
+		m_pAnimController->SetTrackWeight(0, 0.0f);
+		m_pAnimController->SetTrackWeight(1, 1.0f);
+
+		SAFE_RELEASE(pPrevAnimSet);
+
+		m_fPassedBlendTime = 0.0f;
+	}
+
+	m_pAnimController->SetTrackAnimationSet(0, pNextAnimSet);
+	m_pAnimController->ResetTime();
+
+	SAFE_RELEASE(pNextAnimSet);
+}
---
+++ Class_3_DX3D/DX3D/TEAM_TEX.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "IUnitObject.h"
+
+class TEAM_TEX : public IUnitObject
+{
+
+protected:
+	LPD3DXFRAME					m_pRootFrame;
+	LPD3DXANIMATIONCONTROLLER	m_pAnimController;
+
+	int							m_animIndex;
+	float						m_fBlendTime;
+	float						m_fPassedBlendTime;
+
+	LPD3DXMESH					m_pSphereMesh;
+	bool						m_bWireFrame;
+	bool						m_bDrawFrame;
+	bool						m_bDrawSkeleton;
+	int							m_numFrame;
+	int							m_numMesh;
+
+	int							m_state;
+
+	bool						m_HandsOption;
+	float						angle;
+
+	D3DXMATRIXA16				matT, matS, matR;
+
+
+public:
+	TEAM_TEX();
+	~TEAM_TEX();
+
+	// IUnitObject을(를) 통해 상속됨
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render() override;
+
+	void Load(LPCTSTR path, LPCTSTR filename);
+	void SetPos(D3DXVECTOR3 pos) { m_pos = pos; }
+	void SetState(int state) { m_state = state; }
+	void SetMatS(D3DXMATRIXA16 S) { matS = S; }
+	void SetMatR(D3DXMATRIXA16 R) { matR = R; }
+	void SetMatT(D3DXMATRIXA16 T) { matT = T; }
+
+
+private:
+	void SetupBoneMatrixPointers(LPD3DXFRAME pFrame);
+	void SetupBoneMatrixPointersOnMesh(LPD3DXMESHCONTAINER pMeshContainerBase);
+
+	void UpdateAnim();
+	void UpdateFrameMatrices(LPD3DXFRAME pFrame, LPD3DXFRAME pParent);
+
+	void DrawFrame(LPD3DXFRAME pFrame);
+	void DrawMeshContainer(LPD3DXFRAME pFrame);
+	void DrawSkeleton(LPD3DXFRAME pFrame, LPD3DXFRAME pParent);
+
+
+public:
+	void SetAnimationIndex(int nIndex, bool isBlend);
+};
