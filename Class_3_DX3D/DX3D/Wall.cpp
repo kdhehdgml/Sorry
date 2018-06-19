bb3d8f961401321781120a1d44db5710bb3a79c6
김종현 <krestine@nate.com>
2018-06-19 13:36:27 +0900

    클래스 2개 추가

---
+++ Class_3_DX3D/DX3D/Wall.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "Wall.h"
+
+
+Wall::Wall()
+{
+}
+
+
+Wall::~Wall()
+{
+}
+
+void Wall::Init()
+{
+}
+
+void Wall::Update()
+{
+}
+
+void Wall::Render()
+{
+}
+
+BoundingBox * Wall::getBoundingBox()
+{
+	return nullptr;
+}
