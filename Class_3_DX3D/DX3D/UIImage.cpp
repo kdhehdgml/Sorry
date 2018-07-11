41079b05bbd5a674bdcdb6983e07fe5273ef1cef
김종현 <krestine@nate.com>
2018-07-11 20:52:14 +0900

    플레이어 미니맵 수정준비작업

--- Class_3_DX3D/DX3D/UIImage.cpp
+++ Class_3_DX3D/DX3D/UIImage.cpp
@@ -6,6 +6,18 @@ UIImage::UIImage(LPD3DXSPRITE pSprite, int uiTag)
 	:IUBaseObject(pSprite, uiTag)
 	,m_pTex(NULL)
 {
+	m_rotAngle = 0;
+	D3DXVECTOR2 center(m_size.x / 2, m_size.y / 2);
+	D3DXMatrixTransformation2D(&m_rotMatrix, NULL, NULL, NULL, &center, m_rotAngle, NULL);
+}
+
+UIImage::UIImage(LPD3DXSPRITE pSprite, float _rotAngle, int uiTag)
+	:IUBaseObject(pSprite, uiTag)
+	, m_pTex(NULL)
+{
+	m_rotAngle = _rotAngle;
+	D3DXVECTOR2 center(m_size.x / 2, m_size.y / 2);
+	D3DXMatrixTransformation2D(&m_rotMatrix, NULL, NULL, NULL, &center, m_rotAngle, NULL);
 }
 
 
@@ -19,7 +31,8 @@ void UIImage::Render()
 	{
 		RECT rect;
 		SetRect(&rect, 0, 0, m_size.x, m_size.y);
-
+		m_matWorld = m_matWorld * m_rotMatrix;
+		//m_pSprite->SetTransform(&m_matWorld);
 		m_pSprite->Draw(m_pTex, &rect, &m_pivot, &m_combinedPos, m_color);
 	}
 
--- Class_3_DX3D/DX3D/UIImage.h
+++ Class_3_DX3D/DX3D/UIImage.h
@@ -5,8 +5,11 @@ class UIImage : public IUBaseObject
 {
 public:
 	LPDIRECT3DTEXTURE9 m_pTex;
+	D3DXMATRIXA16 m_rotMatrix;
+	float m_rotAngle;
 
 	UIImage(LPD3DXSPRITE pSprite, int uiTag = -1);
+	UIImage(LPD3DXSPRITE pSprite, float _rotAngle, int uiTag = -1);
 	virtual ~UIImage();
 
 	virtual void Render() override;
